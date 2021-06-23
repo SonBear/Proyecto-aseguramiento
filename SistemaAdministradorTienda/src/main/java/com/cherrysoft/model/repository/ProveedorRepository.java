afced446d0818ebc77042a30fde9dee9a5a3c1e7
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-06-22 23:37:10 -0500

    Actualización de proveedorRespository

--- SistemaAdministradorTienda/src/main/java/com/cherrysoft/model/repository/ProveedorRepository.java
+++ SistemaAdministradorTienda/src/main/java/com/cherrysoft/model/repository/ProveedorRepository.java
@@ -1,14 +1,7 @@
 package com.cherrysoft.model.repository;
 
-
 import com.cherrysoft.model.data.Proveedor;
-import com.cherrysoft.model.repository.CrudRepository;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Emmanuel Chable
